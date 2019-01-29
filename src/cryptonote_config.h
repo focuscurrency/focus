899bd672ea5f335c7a0e995f7c568e5899c7d23f
bizshady <bizshady@protonmail.com>
2019-01-28 21:34:33 -0700

    gen tx

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -151,14 +151,14 @@ namespace config
 
   uint64_t const CRYPTONOTE_PUBLIC_ADDRESS_BASE58_PREFIX = 0xe8;//fo
   uint64_t const CRYPTONOTE_PUBLIC_INTEGRATED_ADDRESS_BASE58_PREFIX = 0x5b56; //cu
-  uint64_t const CRYPTONOTE_PUBLIC_SUBADDRESS_BASE58_PREFIX = 0x2b; //s
+  uint64_t const CRYPTONOTE_PUBLIC_SUBADDRESS_BASE58_PREFIX = 0x7bb6; //s
   uint16_t const P2P_DEFAULT_PORT = 14565;
   uint16_t const RPC_DEFAULT_PORT = 14566;
   uint16_t const ZMQ_RPC_DEFAULT_PORT = 14567;
   boost::uuids::uuid const NETWORK_ID = { {
       0x12, 0x30, 0xF1, 0xA1, 0x12, 0x71 , 0x61, 0x04 , 0x41, 0x61, 0x17, 0x31, 0x16, 0xA1, 0x00, 0x82
     } }; // 
-  std::string const GENESIS_TX = "010a01ff0001ffffffffffff0f029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd0880712101abf318a3dc8d66a0263839cffce83daa85963a27635a608a2ba9973055bcc8e400";
+  std::string const GENESIS_TX = "010a01ff0001ffffffffffff0f029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd088071210143df26ab4a91b503c332f86d82adff32cbd7c0ef897bb24876addb42e62dbeab00";
 
   uint32_t const GENESIS_NONCE = 10000;
 
