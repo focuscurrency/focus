5518ab88845801cfc7655eed1c874ca5ffd0d292
bizshady <bizshady@protonmail.com>
2019-01-28 21:46:10 -0700

    gen tx fix

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -158,7 +158,7 @@ namespace config
   boost::uuids::uuid const NETWORK_ID = { {
       0x12, 0x30, 0xF1, 0xA1, 0x12, 0x71 , 0x61, 0x04 , 0x41, 0x61, 0x17, 0x31, 0x16, 0xA1, 0x00, 0x82
     } }; // 
-  std::string const GENESIS_TX = "010a01ff0001ffffffffffff0f029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd088071210143df26ab4a91b503c332f86d82adff32cbd7c0ef897bb24876addb42e62dbeab00";
+  std::string const GENESIS_TX = "010141ff0001ffffffffffff0f029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd088071210143df26ab4a91b503c332f86d82adff32cbd7c0ef897bb24876addb42e62dbeab00";
 
   uint32_t const GENESIS_NONCE = 10000;
 
